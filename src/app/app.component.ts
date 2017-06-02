8244ff87b1dc3ca3efce268732f32016ae046710
Pierre-Jean Baraud <pierre-jean.baraud@amundi.com>
2017-06-02 14:08:33 +0100

    List hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   template: `<h1>Tour of heroes</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id:</label>{{hero.id}}</div>
-  <div><label>name:</label>
-    <input [(ngModel)]="hero.name" placeholder="name" />
-  </div>`,
+  <h2>My heroes</h2>
+  <ul class="heroes">
+    <li *ngFor="let hero of heroes">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
+  </ul>`,
 })
-export class AppComponent  { hero: Hero = { id: 1, name: 'Windstorm' }; }
+export class AppComponent  {
+  heroes = HEROES;
+  hero: Hero = { id: 1, name: 'Windstorm' }; }
