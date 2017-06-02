02320e3aa3a09a099870805e87a63b25febd2ee7
Pierre-Jean Baraud <pierre-jean.baraud@amundi.com>
2017-06-02 16:49:09 +0100

    Edit selected hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,15 @@ const HEROES: Hero[] = [
   template: `<h1>Tour of heroes</h1>
   <h2>My heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
-  </ul>`,
+  </ul>
+  <div *ngIf="selectedHero">
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>id: </label>{{selectedHero.id}}</div>
+  <div><label>name: </label> <input [(ngModel)]="selectedHero.name" placeholder="name" /></div>
+  </div>`,
   styles: [`
     .selected {
   background-color: #CFD8DC !important;
@@ -75,4 +80,9 @@ const HEROES: Hero[] = [
 })
 export class AppComponent  {
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
