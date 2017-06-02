6edcafe5cbb23122f69abd2349669011d566bca4
Pierre-Jean Baraud <pierre-jean.baraud@amundi.com>
2017-06-02 11:50:02 +0100

    Edit Hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Hero } from './hero';
   template: `<h1>Tour of heroes</h1>
   <h2>{{hero.name}} details!</h2>
   <div><label>id:</label>{{hero.id}}</div>
-  <div><label>name:</label>{{hero.name}}</div>`,
+  <div><label>name:</label>
+    <input [(ngModel)]="hero.name" placeholder="name" />
+  </div>`,
 })
 export class AppComponent  { hero: Hero = { id: 1, name: 'Windstorm' }; }
