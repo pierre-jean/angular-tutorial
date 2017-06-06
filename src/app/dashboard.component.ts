8ce99d3361ec5bf776b52fb57b374c3546bbcc65
Pierre-Jean Baraud <pierre-jean.baraud@amundi.com>
2017-06-06 16:42:12 +0100

    Styling Dashboard

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -5,13 +5,14 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService){ }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
