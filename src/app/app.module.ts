88e068359751d1a91624149449ae96135c0620c3
Pierre-Jean Baraud <pierre-jean.baraud@amundi.com>
2017-06-06 16:53:43 +0100

    Create Routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroComponent }  from './hero.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import {DashboardComponent} from './dashboard.component';
+import {RoutingModule} from './routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([{
-    path: 'heroes',
-    component: HeroComponent
-  }, {
-    path: 'dashboard',
-    component: DashboardComponent
-  }, {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-
-  }, {
-    path: 'detail/:id',
-    component: HeroDetailComponent
-  }]) ],
+  imports:      [ BrowserModule, FormsModule, RoutingModule],
   declarations: [ AppComponent, HeroDetailComponent, HeroComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ],
   providers: [HeroService]
