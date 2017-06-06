f033bfcc29908346ef092d2407a3e0cabba171af
Pierre-Jean Baraud <pierre-jean.baraud@amundi.com>
2017-06-06 11:56:12 +0100

    Create Hero Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroComponent }  from './hero.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import {HeroService} from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroComponent ],
   bootstrap:    [ AppComponent ],
   providers: [HeroService]
 })
